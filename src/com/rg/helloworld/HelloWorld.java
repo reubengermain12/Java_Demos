5f40cef09d1fa47ef180055d37c07c9ec36446c6
Germain712 <102153309+Germain712@users.noreply.github.com>
2022-10-12 16:03:48 +0100

    2nd commit

--- src/com/rg/helloworld/HelloWorld.java
+++ src/com/rg/helloworld/HelloWorld.java
@@ -2,8 +2,13 @@ package com.rg.helloworld;
 
 public class HelloWorld {
 	
+	public static void printMessage() {
+		String message = "Hello World!";
+		System.out.println(message);
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		printMessage();
 	}
 
 }
