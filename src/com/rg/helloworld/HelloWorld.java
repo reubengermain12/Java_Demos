f091b1864c5dc1db66d0056641da1a7bfeb8b252
Germain712 <102153309+Germain712@users.noreply.github.com>
2022-10-20 13:52:15 +0100

    Removed comments

--- src/com/rg/helloworld/HelloWorld.java
+++ src/com/rg/helloworld/HelloWorld.java
@@ -3,22 +3,14 @@ package com.rg.helloworld;
 public class HelloWorld {
 
   public static void main(String[] args) {
-//      printMessage("This is my message");
-//      printMessage("A different message");
 
     String result = returnString("Original Value");
     System.out.println(result);
   }
 
   public static void printMessage(String myMessage) {
-//    DataType variableName = value;
-//    String myMessage = "Hello world";
 
-//    println is used to print text to new lines
     System.out.println(myMessage);
-
-//    print is used to print text to the same line
-//    System.out.print("Hello World");
   }
 
   public static String returnString(String myMessage) {
