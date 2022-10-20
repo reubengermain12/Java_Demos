c44d5b127427c9f50e3e0de4abf5e9621f911ea5
Germain712 <102153309+Germain712@users.noreply.github.com>
2022-10-20 13:18:13 +0100

    Added some examples with some comments

--- src/com/rg/helloworld/HelloWorld.java
+++ src/com/rg/helloworld/HelloWorld.java
@@ -1,14 +1,28 @@
 package com.rg.helloworld;
 
 public class HelloWorld {
-	
-	public static void printMessage() {
-		String message = "Hello World!";
-		System.out.println(message);
-	}
-	
-	public static void main(String[] args) {
-		printMessage();
-	}
+
+  public static void main(String[] args) {
+//      printMessage("This is my message");
+//      printMessage("A different message");
+
+    String result = returnString("Original Value");
+    System.out.println(result);
+  }
+
+  public static void printMessage(String myMessage) {
+//    DataType variableName = value;
+//    String myMessage = "Hello world";
+
+//    println is used to print text to new lines
+    System.out.println(myMessage);
+
+//    print is used to print text to the same line
+//    System.out.print("Hello World");
+  }
+
+  public static String returnString(String myMessage) {
+    return myMessage + " I've added something to this!!!";
+  }
 
 }
